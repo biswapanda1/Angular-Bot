2252b94fafd0def258d19fc82ce49a4065c6da30
bunucool <biswa.avit@gmail.com>
2020-03-12 14:25:13 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from "@angular/core";
-
+import { Component,OnInit } from "@angular/core";
+import { Router } from '@angular/router';
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = "Angular";
   address = "test";
+  isLogin = null;
+constructor(private router:Router) { }
+   ngOnInit() {
+   this.isLogin = localStorage.getItem('isLogin');
+   if(this.isLogin == null){
+this.router.navigateByUrl('/login')
+   }else{
+     this.router.navigateByUrl('/')
+   }
+  }
   
 }
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,6 +2,7 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ChatInputComponent } from './chat-input/chat-input.component';
 import { ChatResultComponent } from './chat-result/chat-result.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [    
@@ -10,7 +11,8 @@ import { ChatResultComponent } from './chat-result/chat-result.component';
     RouterModule.forRoot([
        { path: 'input', component: ChatInputComponent },
        { path: 'result', component: ChatResultComponent },
-       { path: '', component: ChatInputComponent }
+       { path: '', component: ChatInputComponent },
+         { path: 'login', component: LoginComponent }
 
       ])
   ],
