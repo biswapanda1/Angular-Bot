6efc3ea0aca691fa67cd25418abeccc87da0bd29
bunucool <biswa.avit@gmail.com>
2020-03-13 15:12:09 +0530

    3 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-  loginForm=new FormGroup({
-    userName: new FormControl('',Validators.required),
-    password: new FormControl('',Validators.required)
+  ngOnInit() {}
+  loginForm = new FormGroup({
+    userName: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required])
   });
-  validateLogin=()=>{
+  validateLogin = () => {
     console.log(this.loginForm.value);
   };
+  get userName() {
+    return this.loginForm.get("userName");
+  }
+  get password() {
+    return this.loginForm.get("password");
+  }
 }
